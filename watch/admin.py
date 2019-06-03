abe9efac0f6f068da2c15f6f71d8b26dad6e2b4e
amiinegal <you@example.com>
2019-06-03 12:54:30 +0300

    create auth

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Neighbourhood, Business, Profile, Post
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
