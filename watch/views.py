d7a7f2075d6946c69084398ef5522d5358b87717
amiinegal <you@example.com>
2019-06-03 12:53:08 +0300

    create view for home page

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+@login_required(login_url='/accounts/login/')
+def register(request):
+    if request.method == 'POST':
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                redirect('login.html')
+    else:
+        form=RegistrationForm()
+        return render(request, 'registration/registration_form.html', {'form':form})    
