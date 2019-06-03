a81439dcfe6617e01936d80933478ccc77546acb
amiinegal <you@example.com>
2019-06-03 12:53:23 +0300

    create required models

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.TextField(max_length=800)
+    profile_pic = models.ImageField(upload_to='profile/')
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length = 100)
+    location_name = models.CharField(max_length = 25)
+    occupants_count = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='neighimage/', null=True)
+    admin = models.ForeignKey(Profile, related_name='hoods', null=True) 
+
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+
+class Business(models.Model):
+    name = models.CharField(max_length= 80)
+    description = models.TextField()
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name='businesses')
+    profile = models.ForeignKey(Profile, related_name='profiles')
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        business = cls.objects.filter(title__icontains=search_term)
+        return business
+
+class Post(models.Model):
+    profile = models.ForeignKey(Profile, related_name='profile')
+    post = models.CharField(max_length=30)
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name='posts')
